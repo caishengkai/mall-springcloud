faca50b29d8bfad414a5a9201eaf9c7491b52274
caishengkai <312039449@qq.com>
2020-04-04 11:17:35 +0800

    新建mall-common、mall-mbg模块，生成用户model和mapper

---
+++ mall-mbg/src/main/java/com/csk/mall/mapper/UmsRoleResourceRelationMapper.java
@@ -0,0 +1,30 @@
+package com.csk.mall.mapper;
+
+import com.csk.mall.model.UmsRoleResourceRelation;
+import com.csk.mall.model.UmsRoleResourceRelationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsRoleResourceRelationMapper {
+    long countByExample(UmsRoleResourceRelationExample example);
+
+    int deleteByExample(UmsRoleResourceRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsRoleResourceRelation record);
+
+    int insertSelective(UmsRoleResourceRelation record);
+
+    List<UmsRoleResourceRelation> selectByExample(UmsRoleResourceRelationExample example);
+
+    UmsRoleResourceRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsRoleResourceRelation record, @Param("example") UmsRoleResourceRelationExample example);
+
+    int updateByExample(@Param("record") UmsRoleResourceRelation record, @Param("example") UmsRoleResourceRelationExample example);
+
+    int updateByPrimaryKeySelective(UmsRoleResourceRelation record);
+
+    int updateByPrimaryKey(UmsRoleResourceRelation record);
+}
